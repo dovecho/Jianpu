6967121a5c5f091c28f779929332512854a98f8d
Silas S. Brown <ssb22@cam.ac.uk>
2023-08-06 21:55:14 +0100

    update to upstream v1.696

--- jianpu-ly.py
+++ jianpu-ly.py
@@ -2,7 +2,7 @@
 # (can be run with either Python 2 or Python 3)
 
 # Jianpu (numbered musical notaion) for Lilypond
-# v1.694 (c) 2012-2023 Silas S. Brown
+# v1.696 (c) 2012-2023 Silas S. Brown
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -188,7 +188,6 @@ def score_end(**headers):
     else: ret += r"\layout{}"
     return ret + " }"
 
-uniqCount = 0
 def uniqName():
     global uniqCount
     r = str(uniqCount) ; uniqCount += 1
@@ -300,7 +299,7 @@ placeholders = {
     '7':'b',
     '-':'r'}
 
-class notehead_markup:
+class NoteheadMarkup:
   def __init__(self):
       self.defines_done = {} ; self.initOneScore()
   def initOneScore(self):
@@ -531,8 +530,6 @@ class notehead_markup:
     else: b4last,aftrlast = "",""
     if inRestHack: ret += " } "
     return b4last,aftrlast0+aftrlast,ret, need_space_for_accidental, nBeams,octave
-
-notehead_markup = notehead_markup()
 
 def parseNote(word):
     if word==".": word = "-" # (for not angka, TODO: document that this is now acceptable as an input word?)
@@ -1103,7 +1100,8 @@ def getLY(score,headers=None):
 
 def process_input(inDat):
  ret = []
- global scoreNo, western, has_lyrics, midi, not_angka, maxBeams
+ global scoreNo, western, has_lyrics, midi, not_angka, maxBeams, uniqCount, notehead_markup
+ uniqCount = 0 ; notehead_markup = NoteheadMarkup()
  scoreNo = 0 # incr'd to 1 below
  western = False
  for score in re.split(r"\sNextScore\s"," "+inDat+" "):
